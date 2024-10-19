87c063ee9dc58c11c06c66f63df849e66b399d30
sebasion9 <kansebakk@gmail.com>
2024-10-19 22:56:09 +0200

    debug packet data reading udp payload correctly reading tcp packets lacks of payload

--- pkta.c
+++ pkta.c
@@ -1,3 +1,4 @@
+#include <linux/skbuff.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -5,75 +6,153 @@
 #include <linux/netfilter_ipv4.h> // netfilter for ipv4
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <linux/udp.h>
 
-#define PACKETS_LEN 500
+#define PACKETS_LEN 500 * 1024
+#define ROWSIZE 16
 
 static unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
-static struct nf_hook_ops nfho;
+static struct nf_hook_ops nfho_in;
+static struct nf_hook_ops nfho_out;
 
-// create second hook for NF_INET_LOCAL_OUT?
-static struct nf_hook_ops nfho = {
+static struct nf_hook_ops nfho_in = {
     .hook = hook_func,
-    //.hooknum = NF_INET_PRE_ROUTING;
-    .hooknum = NF_INET_LOCAL_IN,
+    .hooknum = NF_INET_PRE_ROUTING,
     .pf = PF_INET,                           //IPV4 packets
     .priority = NF_IP_PRI_FIRST,             //set to highest priority over all other hook functions
 };
 
-static unsigned char* dev_mem[PACKETS_LEN];
-static unsigned int dev_mem_len;
+
+static struct nf_hook_ops nfho_out = {
+    .hook = hook_func,
+    .hooknum = NF_INET_POST_ROUTING,
+    .pf = PF_INET,                           //IPV4 packets
+    .priority = NF_IP_PRI_FIRST,             //set to highest priority over all other hook functions
+};
+
+//static unsigned char packets_buff[PACKETS_LEN];
+static unsigned int tcp_packets_len;
 
 static unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-    struct iphdr *ip_header = ip_hdr(skb);
-    struct tcphdr *tcp_header;
-    unsigned char *data;
-    unsigned int data_len;
+    struct iphdr *iphdr;
+    struct tcphdr *tcphdr;
+    struct udphdr *udphdr;
+    unsigned char* tail;
+    int len;
+    u16 sport, dport;
+    u32 saddr, daddr;
 
-    // handle other protocols too
-    if(ip_header->protocol == IPPROTO_TCP) {
-        pr_info("TCP Packet\n");
-        tcp_header = tcp_hdr(skb);
-        pr_info("source port: %u\n", tcp_header->source);
-        pr_info("dest port: %u\n", tcp_header->dest);
-        data = skb->data;
-        data_len = skb->len;
-        pr_info("PACKET LEN: %d\n", data_len);
-
-        if(dev_mem_len >= PACKETS_LEN) {
-            for(int i = 0; i < PACKETS_LEN - 1; i++) { // -1 for skipping last packet
-                dev_mem[i] = dev_mem[i + 1];
-            }
-            dev_mem[PACKETS_LEN - 1] = data;
-
-        }
-        if(dev_mem_len < PACKETS_LEN) {
-            dev_mem[dev_mem_len] = data;
-        }
-        dev_mem_len++;
-        pr_info("device memory array length: %d", dev_mem_len);
-
-        // then when read called on char device defined, somehow retrieve the packets
-        // take in consideration data races?
-        //
-        // drops packet
-        //return NF_DROP;
-
+    // packet is empty, some problem occured, skip the packet
+    if(!skb) {
+        return NF_ACCEPT;
     }
+
+    iphdr = ip_hdr(skb);
+
+    if(iphdr->protocol == IPPROTO_TCP) {
+        tcphdr = tcp_hdr(skb);
+        // convert network endiannes to host endiannes
+        saddr = iphdr->saddr;
+        daddr = iphdr->daddr;
+        sport = tcphdr->source;
+        dport = tcphdr->dest;
+
+        tail = skb_tail_pointer(skb);
+
+        printk(KERN_INFO "tcp packet route: %pI4h:%d -> %pI4h:%d\n", &saddr, sport, &daddr, dport);
+        printk(KERN_INFO "skb fields:\n");
+        printk(KERN_CONT "len: %d\t", skb->len);
+        printk(KERN_CONT "len: %d\t", skb->data_len);
+        printk(KERN_CONT "mac_len: %d\t", skb->mac_len);
+        printk(KERN_CONT "hdr_len: %d\t", skb->hdr_len);
+        printk(KERN_CONT "\n\n");
+        printk(KERN_CONT "tail: %p\t", tail);
+        printk(KERN_CONT "head: %p\t", skb->head);
+        printk(KERN_CONT "data: %p\t", skb->data);
+        printk(KERN_CONT "tail no pointer: %d\t", skb->tail);
+        printk(KERN_CONT "end: %d\t", skb->end);
+
+        printk(KERN_CONT "\n\n");
+        printk(KERN_CONT "attempt to read data\n");
+
+
+        len = skb->len;
+        for(int i = 0; i < len; i++) {
+            printk(KERN_CONT "%02x\t", skb->data[i]);
+            if(i != 0 && i % ROWSIZE == 0)
+                printk(KERN_CONT "\n");
+        }
+        printk(KERN_CONT "\n\n");
+        for(int i = 0; i < len; i++) {
+            printk(KERN_CONT "%c\t", skb->data[i]);
+            if(i != 0 && i % ROWSIZE == 0)
+                printk(KERN_CONT "\n");
+        }
+
+
+        return NF_ACCEPT;
+    }
+    if(iphdr->protocol == IPPROTO_UDP) {
+        udphdr = udp_hdr(skb);
+        // convert network endiannes to host endiannes
+        saddr = iphdr->saddr;
+        daddr = iphdr->daddr;
+        sport = udphdr->source;
+        dport = udphdr->dest;
+
+        tail = skb_tail_pointer(skb);
+
+        printk(KERN_INFO "tcp packet route: %pI4h:%d -> %pI4h:%d\n", &saddr, sport, &daddr, dport);
+        printk(KERN_INFO "skb fields:\n");
+        printk(KERN_CONT "len: %d\t", skb->len);
+        printk(KERN_CONT "len: %d\t", skb->data_len);
+        printk(KERN_CONT "mac_len: %d\t", skb->mac_len);
+        printk(KERN_CONT "hdr_len: %d\t", skb->hdr_len);
+        printk(KERN_CONT "\n\n");
+        printk(KERN_CONT "tail: %p\t", tail);
+        printk(KERN_CONT "head: %p\t", skb->head);
+        printk(KERN_CONT "data: %p\t", skb->data);
+        printk(KERN_CONT "tail no pointer: %d\t", skb->tail);
+        printk(KERN_CONT "end: %d\t", skb->end);
+
+        printk(KERN_CONT "\n\n");
+        printk(KERN_CONT "attempt to read data\n");
+
+
+        len = skb->len;
+        for(int i = 0; i < len; i++) {
+            printk(KERN_CONT "%02x\t", skb->data[i]);
+            if(i != 0 && i % ROWSIZE == 0)
+                printk(KERN_CONT "\n");
+        }
+        printk(KERN_CONT "\n\n");
+        for(int i = 0; i < len; i++) {
+            printk(KERN_CONT "%c\t", skb->data[i]);
+            if(i != 0 && i % ROWSIZE == 0)
+                printk(KERN_CONT "\n");
+        }
+
+
+        return NF_ACCEPT;
+    }
+
     return NF_ACCEPT;
 }
 
 
 static int __init kmod_init(void)
 {
-    dev_mem_len = 0;
-    pr_info("NF module loaded, NF hook registered");
-    nf_register_net_hook(&init_net, &nfho);
+    tcp_packets_len = 0;
+    pr_info("NF module loaded, NF hooks registered");
+    nf_register_net_hook(&init_net, &nfho_in);
+    nf_register_net_hook(&init_net, &nfho_out);
     return 0;
 }
 
 static void __exit kmod_exit(void)
 {
-    nf_unregister_net_hook(&init_net, &nfho);
+    nf_unregister_net_hook(&init_net, &nfho_in);
+    nf_unregister_net_hook(&init_net, &nfho_out);
     pr_info("NF module unloaded");
 }
 
